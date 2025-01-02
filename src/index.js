8eaf32ea7103c0dd57413bfc3f4d2e87023545ae
Alekhya Suresh Oruganti <138512768+AlekhyaSuresh@users.noreply.github.com>
2025-01-02 10:58:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Add basename to BrowserRouter */}
+    <BrowserRouter basename="/react-calender"> 
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
